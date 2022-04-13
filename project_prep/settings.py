158417574199dffc6ef044d79f33dccb6f65dc4e
InaKuku <inakuku@gmail.com>
2022-04-13 22:35:44 +0300

    Remove static cache

--- project_prep/settings.py
+++ project_prep/settings.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR/'mediafiles'
 MEDIA_URL = '/media/'
